1c73fa96dbc996d3530c8534c09fc908e89e4847
Furkan Türkan <51923824+genjitsuCode@users.noreply.github.com>
2022-01-17 16:26:15 +0300

    zindex update

--- pages/works/flutprogramming.js
+++ pages/works/flutprogramming.js
@@ -1,9 +1,20 @@
+import {
+    Container,
+    Badge
+  } from '@chakra-ui/react'
   import Layout from '../../components/layouts/article'
+  import { Title } from '../../components/work'
   
   const Work = () => (
     <Layout title="amembo">
+      <Container>
+        <Title>
+          Variables Programming <Badge>2022</Badge>
+        </Title>
       
-      <iframe src="https://flutter.atakancelik.com.tr/#/" width="100%" height="700px"></iframe>
+  
+      </Container>
+      <iframe src="https://flutter.atakancelik.com.tr/#/" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe>
     </Layout>
   )
   
