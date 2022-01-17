b545d19e8e8aeb0f13806c0355a523b6faa4c6fd
Furkan Türkan <51923824+genjitsuCode@users.noreply.github.com>
2022-01-17 16:55:37 +0300

    style updatre

--- pages/works/flutprogramming.js
+++ pages/works/flutprogramming.js
@@ -4,9 +4,10 @@
   const Work = () => (
     <Layout title="amembo">
       
-      <iframe src="https://flutter.atakancelik.com.tr/#/" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe>
+      <iframe src="https://flutter.atakancelik.com.tr/#/" style={{position:'fixed', top:'0', left:'0', bottom:'0', right:'0', width:'100%', height:'900px', border:'none', margin:'0', padding:'0', overflow:'hidden', zIndex:'999999'}}></iframe>
     </Layout>
   )
+  
   
   export default Work
   
