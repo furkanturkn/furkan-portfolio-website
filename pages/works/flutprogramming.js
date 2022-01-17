672bba8e71559140c3baf53ca8b46e4e9cd47a86
Furkan Türkan <51923824+genjitsuCode@users.noreply.github.com>
2022-01-17 15:30:49 +0300

    flutter respo

--- pages/works/flutprogramming.js
+++ pages/works/flutprogramming.js
@@ -1,19 +1,8 @@
-import {
-    Container,
-    Badge
-  } from '@chakra-ui/react'
   import Layout from '../../components/layouts/article'
-  import { Title } from '../../components/work'
   
   const Work = () => (
     <Layout title="amembo">
-      <Container>
-        <Title>
-          Variables Programming <Badge>2022</Badge>
-        </Title>
       
-  
-      </Container>
       <iframe src="https://flutter.atakancelik.com.tr/#/" width="100%" height="700px"></iframe>
     </Layout>
   )
