fa23cfced04467556410ba7a91f309374024001d
Furkan Türkan <51923824+genjitsuCode@users.noreply.github.com>
2022-01-24 00:02:13 +0300

    change iframe

--- pages/works/flutprogramming.js
+++ pages/works/flutprogramming.js
@@ -4,7 +4,7 @@
   const Work = () => (
     <Layout title="amembo">
       
-      <iframe src="https://flutter.atakancelik.com.tr/#/" style={{position:'fixed', top:'0', left:'0', bottom:'0', right:'0', width:'100%', height:'100%', border:'none', margin:'0', padding:'0', overflow:'hidden', zIndex:'999999'}}></iframe>
+      <iframe src="https://genjitsucode.github.io/variablesweb/#/" style={{position:'fixed', top:'0', left:'0', bottom:'0', right:'0', width:'100%', height:'100%', border:'none', margin:'0', padding:'0', overflow:'hidden', zIndex:'999999'}}></iframe>
     </Layout>
   )
   
